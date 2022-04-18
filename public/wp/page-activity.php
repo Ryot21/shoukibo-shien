2a63bb7fddf0dc3af346b3668d9b1ef157ef954f
Ryota Maeda <r.maeda@jaythree.com>
2022-04-18 09:55:25 +0900

    活動内容 > スマホVerとPCVerの教示切り替え

--- public/wp/page-activity.php
+++ public/wp/page-activity.php
@@ -19,7 +19,9 @@ Template Name: ACTIVITY template
           <h2 class="p-activity-block_title">有益な情報発信</h2>
           <div class="p-activity-block-wrap">
             <div class="p-activity-block_img activity-1">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/jouhou-kyouyu@2x.png" class="" alt="会員様に向けた有益な情報発信">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/jouhou-kyouyu@2x.png" class="PC" alt="会員様に向けた有益な情報発信">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-2-sp@2x.png" class="SP" alt="会員様に向けた有益な情報発信">
+
             </div>
           </div>
           <!-- <p class="p-activity-block_text">
@@ -31,7 +33,9 @@ Template Name: ACTIVITY template
           <h2 class="p-activity-block_title">財務相談</h2>
           <div class="p-activity-block-wrap">
             <div class="p-activity-block_img activity-2">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/cosutomondai@2x.png" class="" alt="コストや財務問題の解決">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/cosutomondai@2x.png" class="PC" alt="コストや財務問題の解決">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-1-sp@2x.png" class="SP" alt="コストや財務問題の解決">
+
             </div>
           </div>
 
@@ -41,7 +45,11 @@ Template Name: ACTIVITY template
           <h2 class="p-activity-block_title">提携先の紹介と法人化サポート</h2>
           <div class="p-activity-block-wrap">
             <div class="p-activity-block_img activity-3">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/support@2x.png" class="" alt="提携先の紹介と法人化サポート">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/support@2x.png" class="PC" alt="提携先の紹介と法人化サポート">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-3-sp@2x.png" class="SP" alt="提携先の紹介">
+            </div>
+            <div class="p-activity-block_img activity-3 SP">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-4-sp@2x.png" class="SP" alt="法人化サポート">
             </div>
           </div>
 
