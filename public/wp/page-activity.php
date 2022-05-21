a71bdcd5c16842d3b20defcc71bf8df6241e337f
Ryota Maeda <r.maeda@jaythree.com>
2022-05-21 10:09:09 +0900

    活動概要 > スマホVer画像の画質向上

--- public/wp/page-activity.php
+++ public/wp/page-activity.php
@@ -20,7 +20,7 @@ Template Name: ACTIVITY template
           <div class="p-activity-block-wrap">
             <div class="p-activity-block_img activity-1">
               <img src="<?php bloginfo('template_url'); ?>/images/activity/jouhou-kyouyu-kaizen-min.png" class="PC" alt="会員様に向けた有益な情報発信">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-2-sp-kaizen-min.png" class="SP" alt="会員様に向けた有益な情報発信">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-2-sp@2x-min.png" class="SP" alt="会員様に向けた有益な情報発信">
 
             </div>
           </div>
@@ -31,7 +31,7 @@ Template Name: ACTIVITY template
           <div class="p-activity-block-wrap">
             <div class="p-activity-block_img activity-2">
               <img src="<?php bloginfo('template_url'); ?>/images/activity/cosutomondai-kaizen-min.png" class="PC" alt="コストや財務問題の解決">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-1-sp-kaizen-min.png" class="SP" alt="コストや財務問題の解決">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-1-sp@2x-min.png" class="SP" alt="コストや財務問題の解決">
 
             </div>
           </div>
@@ -39,14 +39,14 @@ Template Name: ACTIVITY template
         </section>
         <!-- 提携先の紹介と法人化サポート -->
         <section id="activity-3" class="p-activity-block section-block">
-          <h2 class="p-activity-block_title">提携先の紹介と法人化サポート</h2>
+          <h2 class="p-activity-block_title">提携先の紹介と<br class="SP">法人化サポート</h2>
           <div class="p-activity-block-wrap">
             <div class="p-activity-block_img activity-3">
               <img src="<?php bloginfo('template_url'); ?>/images/activity/support-kaizen-min.png" class="PC" alt="提携先の紹介と法人化サポート">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-3-sp-kaizen-min.png" class="SP" alt="提携先の紹介">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-3-sp@2x-min.png" class="SP" alt="提携先の紹介">
             </div>
             <div class="p-activity-block_img activity-3 SP">
-              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-4-sp-kaizen-min.png" class="SP" alt="法人化サポート">
+              <img src="<?php bloginfo('template_url'); ?>/images/activity/activity-4-sp@2x-min.png" class="SP" alt="法人化サポート">
             </div>
           </div>
 
